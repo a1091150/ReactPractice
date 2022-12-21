31d34a7f6fc39008c590b4b14053c38d6c2952aa
DokiDokiPB <20497934+a1091150@users.noreply.github.com>
2022-12-21 23:46:42 +0800

    ITHome Day 16, but show no content

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-let menuItemWording = [
-    "Like的發問",
-    "Like的回答",
-    "Like的文章",
-    "Like的留言"
-];
+// https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import MenuPage from "./page/MenuPage";
+import FormPage from "./page/FormPage";
 
+const Layout = (props) => {
+    return (
+        <>
+            <nav>
+                <Link to="/">點我連到第一頁</Link>
+                <Link to="/form" style={{ marginLeft: "20px" }}>點我連到第二頁</Link>
+            </nav>
+            {props.children}
+        </>
+    )
+}
 
-import MenuItem2 from './component/MenuItem2';
-import Menu from './component/Menu';
+const App = () => {
+    return (
+        <HashRouter>
+            <Routes>
+                <Layout>
+                    <Route exact path="/" component={<MenuPage></MenuPage>} />
+                    <Route path="/form" component={<FormPage></FormPage>} />
+                </Layout>
+            </Routes>
+        </HashRouter>
+    );
+}
 
-let menuItemArr3 = menuItemWording.map((wording) => <MenuItem2 text={wording} />);
 ReactDOM.render(
-    <Menu title={"這是標題!"}>{menuItemArr3}</Menu>,
-    document.getElementById('root5')
-)
+    <App />,
+    document.getElementById('root')
+);
---
+++ src/page/FormPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormPage = () => {
+    return <li>This is Form Page!</li>;
+}
+
+export default FormPage;
